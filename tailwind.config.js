fd51cd078fcd3b09d16af0d4112a991320de8118
YuryT <yury.tyn@hotmail.com>
2021-03-07 18:06:33 +0300

    enable tailwind purge options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  darkMode: false, // or 'media' or 'class'
-  //purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
