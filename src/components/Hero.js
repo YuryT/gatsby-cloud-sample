4d940ef8f53f69da335a6e565961e7653faf8980
YuryT <yury.tyn@hotmail.com>
2021-03-09 14:14:20 +0300

    use btns from contentful

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -34,20 +34,26 @@ const MainText = (props) => (
       <h2 className="text-4xl tracking-tight font-extrabold text-primaryColor-default sm:text-5xl md:text-6xl">
         <span className="block xl:inline">{props.header}</span>
       </h2>
-              <h3 className="tracking-tight text-3xl sm:text-4xl md:text-5xl "><span
-          className="block text-primaryColor-default xl:inline">{props.subHeader}</span>
+      <h3 className="tracking-tight text-3xl sm:text-4xl md:text-5xl ">
+        <span
+        className="block text-primaryColor-default xl:inline"
+        >
+          {props.subHeader}
+        </span>
       </h3>
-      <p
-        className="mt-3 text-base text-pseudoWhite-hover sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+      <div
+        className="mt-3 text-base text-pseudoWhite-hover sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+      >
         <div dangerouslySetInnerHTML={{__html: props.description.childMarkdownRemark.html}} />
-      </p>
-      <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-        <div className="">
-          <Button url='#' label='Заказать'/>
-        </div>
-        <div className="mt-3 sm:mt-0 sm:ml-3">
-          <Button url='#' label='Звони сейчас'/>
-        </div>
+      </div>
+      <div className="mt-5 sm:mt-8 sm:flex sm:justify-between">
+        {props.buttonsData.edges.map((btnData, i)=>(
+          <Button
+            url={btnData.node.url}
+            key={i}
+            label={btnData.node.label}
+          />
+        ))}
       </div>
     </div>
   </main>
