92b47268c3cbee002af61c152c2031a0338c8fad
YuryT <yury.tyn@hotmail.com>
2021-03-09 14:13:38 +0300

    use AnchorLink in btn

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 const Button = (props) => {
-  return (<a href={props.url}
-             className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-pseudoWhite-default hover:text-pseudoWhite-hover bg-primaryColor-default hover:bg-primaryColor-hover md:py-4 md:text-lg md:px-10">
+  return (<AnchorLink to={props.url}
+             className=" w-full flex items-center justify-center px-8 py-3
+             border border-transparent text-base font-medium rounded-md
+             md:py-4 md:text-lg md:px-10 mt-3 sm:mt-0 sm:ml-3 md:max-w-xs
+             text-pseudoWhite-default hover:text-pseudoWhite-hover
+             bg-primaryColor-default hover:bg-primaryColor-hover
+             ">
       {props.label}
-    </a>
+    </AnchorLink>
   );
 };
 
